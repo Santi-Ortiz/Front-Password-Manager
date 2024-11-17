e6393fe921556f4d1a6349d5692878314c251b93
Juando26030 <juando02603@gmail.com>
2024-11-16 23:09:10 -0500

    estilos register listos

--- password-manager/src/app/app.component.ts
+++ password-manager/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./session/login/login.component";
+import {RegisterComponent} from "./session/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
