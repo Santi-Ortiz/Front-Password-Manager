35ca37cd72d9e2a94f237dbd4b59ab528ca5d22c
Juando26030 <juando02603@gmail.com>
2024-11-16 23:03:20 -0500

    componentes creados y login diseño listo

---
+++ password-manager/src/app/explore/dashboard-explore/dashboard-explore.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-explore',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-explore.component.html',
+  styleUrl: './dashboard-explore.component.css'
+})
+export class DashboardExploreComponent {
+
+}
---
+++ password-manager/src/app/session/dashboard-session/dashboard-session.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-session',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-session.component.html',
+  styleUrl: './dashboard-session.component.css'
+})
+export class DashboardSessionComponent {
+
+}
