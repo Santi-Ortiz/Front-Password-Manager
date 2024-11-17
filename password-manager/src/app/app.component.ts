92ca8cd8e28095c3daedf024e440595c00fb6b45
Juando26030 <juando02603@gmail.com>
2024-11-16 23:34:25 -0500

    estilos tabla cuentas listos

--- password-manager/src/app/app.component.ts
+++ password-manager/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./session/login/login.component";
 import {RegisterComponent} from "./session/register/register.component";
+import {AccountListComponent} from "./explore/account-list/account-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, AccountListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
