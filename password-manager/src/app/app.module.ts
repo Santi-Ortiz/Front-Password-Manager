599691ae321eed5beff00a2c6137ccace46ada0f
Santiago Ortiz <or.santiago@javeriana.edu.co>
2024-11-20 17:27:40 -0500

    Cambios en front en la tabla de cuentas

--- password-manager/src/app/app.module.ts
+++ password-manager/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [],
