b991e1fe92fa07e328f29f95d68ff5d6442682c3
Juando26030 <juando02603@gmail.com>
2024-11-20 18:37:17 -0500

    componente perfil (para consultar y actualizar) creado

--- password-manager/src/app/app.routes.ts
+++ password-manager/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from "./session/login/login.component";
 import {AccountListComponent} from "./explore/account-list/account-list.component";
 import {RegisterComponent} from "./session/register/register.component";
+import {ProfileComponent} from "./session/profile/profile.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'account-list', component: AccountListComponent },
+  { path: 'my-profile', component: ProfileComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
