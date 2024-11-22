cdac2c852cdeebb537f2e17e00b761c3f2077b27
Santiago Ortiz <or.santiago@javeriana.edu.co>
2024-11-21 19:33:21 -0500

    Agregar fila de la tabla con botón superior de +

--- password-manager/src/app/app.module.ts
+++ password-manager/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 
+import { AccountListComponent } from './explore/account-list/account-list.component';
+import { AccountService } from './services/account.service';
+
 @NgModule({
-  declarations: [],
-  imports: [ HttpClientModule
+  declarations: [
+    AppComponent,
+    AccountListComponent
+  ],
+  imports: [ 
+    HttpClientModule,
+    BrowserModule,
+    FormsModule,
   ],
   bootstrap: [AppComponent]
 })
--- password-manager/src/app/explore/account-list/account-list.component.ts
+++ password-manager/src/app/explore/account-list/account-list.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-account-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule], 
   templateUrl: './account-list.component.html',
-  styleUrl: './account-list.component.css'
+  styleUrls: ['./account-list.component.css']
 })
 export class AccountListComponent {
+  accounts = [
+    { sitio: 'Google', usuario: '9518', contrasena: '6369', url: '01:32:50', info: '01:32:50' },
+    { sitio: 'Twitter', usuario: '7326', contrasena: '10437', url: '00:51:22', info: '01:32:50' },
+  ];
 
+  tempAccounts: any[] = [];
+
+  addTempRow() {
+    this.tempAccounts.push({ sitio: '', usuario: '', contrasena: '', url: '', info: '' });
+  }
+
+  saveRows() {
+    this.accounts = [...this.accounts, ...this.tempAccounts];
+    this.tempAccounts = [];
+  }
 }
